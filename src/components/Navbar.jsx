04a503af6fd01b3564bb75880206540d98dc6e6b
atiqul-islam <imonatikulislam@gmail.com>
2024-12-29 02:01:38 +0600

    Modify Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,7 +11,7 @@ const Navbar = () => {
 
   return (
     <nav className="bg-white text-black sticky top-0 z-50 shadow-md">
-      <div className="max-w-[1440px] mx-auto px-4 sm:px-6 lg:px-8">
+      <div className="max-w-[auto] mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center py-6">
           {/* Logo */}
           <div className="text-base font-bold">
@@ -19,7 +19,7 @@ const Navbar = () => {
               <img
                 src="https://res.cloudinary.com/db5yniogx/image/upload/v1735327489/shantibarilogocloud_a8h82d.png"
                 alt="Shantibari Logo"
-                className="w-20 h-auto cursor-pointer"
+                className="w-28 h-auto cursor-pointer"
               />
             </NavLink>
           </div>
@@ -31,7 +31,7 @@ const Navbar = () => {
               className="text-black focus:outline-none"
             >
               <svg
-                className="w-6 h-6"
+                className="w-7 h-7"
                 fill="none"
                 stroke="currentColor"
                 viewBox="0 0 24 24"
@@ -47,8 +47,8 @@ const Navbar = () => {
             </button>
           </div>
 
-          {/* Navbar Links */}
-          <div className="hidden lg:flex space-x-8 font-roboto text-base">
+          {/* Navbar Links (Desktop View) */}
+          <div className="hidden lg:flex space-x-8 font-roboto text-lg justify-center w-full">
             <NavLink to="/" className="hover:text-gray-400">
               Home
             </NavLink>
@@ -77,57 +77,79 @@ const Navbar = () => {
               Download Brochure
             </NavLink>
           </div>
-
-          {/* Call Us Button */}
-          <div className="hidden lg:block">
-            <a
-              href="tel:+8801325167271"
-              className="bg-green-500 text-white px-8 py-3 rounded-full hover:bg-green-400 text-sm"
-            >
-              Call Us
-            </a>
-          </div>
         </div>
       </div>
 
       {/* Mobile Menu */}
       {isMenuOpen && (
-        <div className="lg:hidden px-6 py-4 space-y-6 bg-white">
-          <NavLink to="/" onClick={closeMenu} className="block text-black text-base">
+        <div className="lg:hidden px-6 py-4 space-y-6 bg-white justify-center w-full transition-all duration-300 ease-in-out transform">
+          <NavLink
+            to="/"
+            onClick={closeMenu}
+            className="block text-black text-lg text-center py-2 hover:text-gray-400"
+          >
             Home
           </NavLink>
-          <NavLink to="/about" onClick={closeMenu} className="block text-black text-base">
+          <NavLink
+            to="/about"
+            onClick={closeMenu}
+            className="block text-black text-lg text-center py-2 hover:text-gray-400"
+          >
             About Us
           </NavLink>
-          <NavLink to="/services" onClick={closeMenu} className="block text-black text-base">
+          <NavLink
+            to="/services"
+            onClick={closeMenu}
+            className="block text-black text-lg text-center py-2 hover:text-gray-400"
+          >
             Services
           </NavLink>
-          <NavLink to="/events" onClick={closeMenu} className="block text-black text-base">
+          <NavLink
+            to="/events"
+            onClick={closeMenu}
+            className="block text-black text-lg text-center py-2 hover:text-gray-400"
+          >
             Events
           </NavLink>
           <NavLink
             to="/gallery-frida-kahlo"
             onClick={closeMenu}
-            className="block text-black text-base"
+            className="block text-black text-lg text-center py-2 hover:text-gray-400"
           >
             Gallery Frida Kahlo
           </NavLink>
-          <NavLink to="/contact" onClick={closeMenu} className="block text-black text-base">
+          <NavLink
+            to="/contact"
+            onClick={closeMenu}
+            className="block text-black text-lg text-center py-2 hover:text-gray-400"
+          >
             Contact Us
           </NavLink>
-          <NavLink to="/audio-visual" onClick={closeMenu} className="block text-black text-base">
+          <NavLink
+            to="/audio-visual"
+            onClick={closeMenu}
+            className="block text-black text-lg text-center py-2 hover:text-gray-400"
+          >
             Audio Visual
           </NavLink>
-          <NavLink to="/media-coverage" onClick={closeMenu} className="block text-black text-base">
+          <NavLink
+            to="/media-coverage"
+            onClick={closeMenu}
+            className="block text-black text-lg text-center py-2 hover:text-gray-400"
+          >
             Media Coverage
           </NavLink>
-          <NavLink to="/brochure" onClick={closeMenu} className="block text-black text-base">
+          <NavLink
+            to="/brochure"
+            onClick={closeMenu}
+            className="block text-black text-lg text-center py-2 hover:text-gray-400"
+          >
             Download Brochure
           </NavLink>
-          <div className="mt-6">
+          <div className="mt-6 flex justify-center">
             <a
               href="tel:+8801325167271"
-              className="block bg-green-500 text-white px-8 py-3 rounded-full hover:bg-green-400 text-base"
+              className="block bg-green-500 text-white px-8 py-3 rounded-full hover:bg-green-400 text-lg"
             >
               Call Us
             </a>
