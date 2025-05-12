21899eaa6abcd0fe46562097bab468fa579cc16e
atiqul-islam <imonatikulislam@gmail.com>
2025-05-12 16:36:14 +0600

    Add new photos in Gallery

--- src/components/PhotoGallery.jsx
+++ src/components/PhotoGallery.jsx
@@ -6,7 +6,51 @@ function PhotoGallery() {
   const [startIndex, setStartIndex] = useState(0); // Index of the first image in the visible range
 
   const photos = [
+
     
+    "https://res.cloudinary.com/db5yniogx/image/upload/v1747043749/005_whcvso.jpg",
+    "https://res.cloudinary.com/db5yniogx/image/upload/v1747043749/046_okqqvs.jpg",
+    "https://res.cloudinary.com/db5yniogx/image/upload/v1747043749/023_ozxsst.jpg",
+    "https://res.cloudinary.com/db5yniogx/image/upload/v1747043749/016_cxy8ew.jpg",
+    "https://res.cloudinary.com/db5yniogx/image/upload/v1747043749/006_l54594.jpg",
+    "https://res.cloudinary.com/db5yniogx/image/upload/v1747043749/002_vf6nky.jpg",
+    "https://res.cloudinary.com/db5yniogx/image/upload/v1747043748/021_rhxth4.jpg",
+    "https://res.cloudinary.com/db5yniogx/image/upload/v1747043748/011_ovs2tn.jpg",
+    "https://res.cloudinary.com/db5yniogx/image/upload/v1747043748/019_bcuwg1.jpg",
+    "https://res.cloudinary.com/db5yniogx/image/upload/v1747043748/022_doczxt.jpg",
+    'https://res.cloudinary.com/db5yniogx/image/upload/v1747043748/025_ux8d6x.jpg',
+    "https://res.cloudinary.com/db5yniogx/image/upload/v1747043749/027_pgffbo.jpg",
+    "https://res.cloudinary.com/db5yniogx/image/upload/v1747043750/038_z1k14x.jpg",
+    "https://res.cloudinary.com/db5yniogx/image/upload/v1747043748/015_iqnjco.jpg",
+    "https://res.cloudinary.com/db5yniogx/image/upload/v1747043748/004_hv20tq.jpg",
+    "https://res.cloudinary.com/db5yniogx/image/upload/v1747043747/039_k7jw8l.jpg",
+    "https://res.cloudinary.com/db5yniogx/image/upload/v1747043747/018_flbcrb.jpg",
+    "https://res.cloudinary.com/db5yniogx/image/upload/v1747043747/003_gzjiwd.jpg",
+    "https://res.cloudinary.com/db5yniogx/image/upload/v1747043747/030_mbibsp.jpg",
+    "https://res.cloudinary.com/db5yniogx/image/upload/v1747043747/035_nslcig.jpg",
+    "https://res.cloudinary.com/db5yniogx/image/upload/v1747043750/029_kkjcir.jpg",
+    "https://res.cloudinary.com/db5yniogx/image/upload/v1747043746/031_xt0s1q.jpg",
+    "https://res.cloudinary.com/db5yniogx/image/upload/v1747043746/036_drx8v3.jpg",
+    "https://res.cloudinary.com/db5yniogx/image/upload/v1747043746/042_xixosv.jpg",
+    "https://res.cloudinary.com/db5yniogx/image/upload/v1747043746/041_e7wxeq.jpg",
+    "https://res.cloudinary.com/db5yniogx/image/upload/v1747043746/044_swi6yp.jpg",
+    "https://res.cloudinary.com/db5yniogx/image/upload/v1747043746/045_u0qk6b.jpg",
+    "https://res.cloudinary.com/db5yniogx/image/upload/v1747043745/010_jfelq0.jpg",
+    "https://res.cloudinary.com/db5yniogx/image/upload/v1747043745/008_wmimve.jpg",
+    "https://res.cloudinary.com/db5yniogx/image/upload/v1747043745/013_g71mwi.jpg",
+    "https://res.cloudinary.com/db5yniogx/image/upload/v1747043745/032_uackqo.jpg",
+    "https://res.cloudinary.com/db5yniogx/image/upload/v1747043745/034_kdsg4c.jpg",
+    "https://res.cloudinary.com/db5yniogx/image/upload/v1747043745/033_lqd7xm.jpg",
+    "https://res.cloudinary.com/db5yniogx/image/upload/v1747043744/007_mc9nca.jpg",
+    "https://res.cloudinary.com/db5yniogx/image/upload/v1747043744/028_vtnvhs.jpg",
+    "https://res.cloudinary.com/db5yniogx/image/upload/v1747043744/009_jpye94.jpg",
+    "https://res.cloudinary.com/db5yniogx/image/upload/v1747043744/020_a10nvl.jpg",
+    "https://res.cloudinary.com/db5yniogx/image/upload/v1747043744/017_equ6en.jpg",
+    "https://res.cloudinary.com/db5yniogx/image/upload/v1747043744/040_csk8az.jpg",
+    "https://res.cloudinary.com/db5yniogx/image/upload/v1747043744/037_cmahnw.jpg",
+    "https://res.cloudinary.com/db5yniogx/image/upload/v1747043743/043_cofftc.jpg",
+    "https://res.cloudinary.com/db5yniogx/image/upload/v1747043743/024_pfgiwl.jpg",
+    "https://res.cloudinary.com/db5yniogx/image/upload/v1747043743/026_epmiux.jpg",
     'https://res.cloudinary.com/db5yniogx/image/upload/v1736939701/valothakacloud15_nsmhgi.jpg',
     'https://res.cloudinary.com/db5yniogx/image/upload/v1736939567/valothakacloud14_o2cvwo.jpg',
     'https://res.cloudinary.com/db5yniogx/image/upload/v1736939508/valothakacloud13_idtbud.jpg',
@@ -39,6 +83,8 @@ function PhotoGallery() {
     'https://res.cloudinary.com/db5yniogx/image/upload/v1735322532/shantibarigallerycloud12_kjfmgc.jpg',
     'https://res.cloudinary.com/db5yniogx/image/upload/v1735322533/shantibarigallerycloud11_qkf1is.jpg',
     'https://res.cloudinary.com/db5yniogx/image/upload/v1735322512/shantibarigallerycloud4_b50nre.jpg',
+    
+
   ];
 
   const changeMainImage = (photo) => {
